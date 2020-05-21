3852921d9877510bbfdd433fb02aa25be747b73f
Daniel Tschinder <duck+github@mailbox.org>
2020-05-21 17:27:51 +0000

    fix(proxy): Migrate to yargs

--- packages/emailonacid-proxy/lib/parse-options.js
+++ packages/emailonacid-proxy/lib/parse-options.js
@@ -1,28 +1,36 @@
 'use strict';
 
-const commander = require('commander');
+const yargs = require('yargs');
 
 function getOptions(argv = []) {
-  return commander
-    .option('-p, --port <port>', 'Port number', parseInt, 8080)
-    .option(
-      '-r, --requests-per-interval <rpi>',
-      'Max requests to make per given interval',
-      parseInt,
-      60
-    )
-    .option(
-      '-i, --interval <interval>',
-      'Time interval to track requests in milliseconds',
-      parseInt,
-      5 * 60e3
-    )
-    .option(
-      '-d, --destination [url]',
-      'Proxy destination',
-      'https://api.emailonacid.com/v5'
-    )
-    .parse(argv);
+  return yargs
+    .option('p', {
+      alias: 'port',
+      type: 'number',
+      description: 'Port number',
+      default: 8080
+    })
+    .option('r', {
+      alias: 'requests-per-interval',
+      type: 'number',
+      description: 'Max requests to make per given interval',
+      default: 60
+    })
+    .option('i', {
+      alias: 'interval',
+      type: 'number',
+      description: 'Time interval to track requests in milliseconds',
+      default: 5 * 60e3
+    })
+    .option('d', {
+      alias: 'destination',
+      type: 'string',
+      description: 'Proxy destination',
+      default: 'https://api.emailonacid.com/v5'
+    })
+    .demandCommand(0, 0)
+    .strict()
+    .argv;
 }
 
 module.exports = getOptions;
